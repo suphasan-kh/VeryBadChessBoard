05afe392a981df34bc984d01a14c470f83175749
Suphasan Khempong <148676084+suphasan-kh@users.noreply.github.com>
2024-04-18 12:05:09 +0700

    Add files via upload

--- boards.js
+++ boards.js
@@ -5,21 +5,44 @@ const realsize = Math.floor(size*0.1);
 document.documentElement.style.setProperty("--getsize", `${realsize}px`);
 document.documentElement.style.setProperty("--getfont", `${realsize/4}px`);
 
+let latestPiece = null;
 let selectedPiece = null;
 let selectedCell = null;
 let canCapture = true;
+let clicked = false;
 
-const pos_dict = {"a8":1, "b8":2, "c8":3, "d8":4, "e8":5, "f8":6, "g8":7, "h8":8,
+let startState = [
+'r','n','b','q','k','b','n','r',
+'p','p','p','p','p','p','p','p',
+'-','-','-','-','-','-','-','-',
+'-','-','-','-','-','-','-','-',
+'-','-','-','-','-','-','-','-',
+'-','-','-','-','-','-','-','-',
+'P','P','P','P','P','P','P','P',
+'R','N','B','Q','K','B','N','R'];
+
+const pos_dictp1 = {
+"a8":1, "b8":2, "c8":3, "d8":4, "e8":5, "f8":6, "g8":7, "h8":8,
 "a7":9, "b7":10, "c7":11, "d7":12, "e7":13, "f7":14, "g7":15, "h7":16,
 "a6":17, "b6":18, "c6":19, "d6":20, "e6":21, "f6":22, "g6":23, "h6":24,
 "a5":25, "b5":26, "c5":27, "d5":28, "e5":29, "f5":30, "g5":31, "h5":32,
 "a4":33, "b4":34, "c4":35, "d4":36, "e4":37, "f4":38, "g4":39, "h4":40,
 "a3":41, "b3":42, "c3":43, "d3":44, "e3":45, "f3":46, "g3":47, "h3":48,
 "a2":49, "b2":50, "c2":51, "d2":52, "e2":53, "f2":54, "g2":55, "h2":56,
-"a1":57, "b1":58, "c1":59, "d1":60, "e1":61, "f1":62, "g1":63, "h1":64
-};
+"a1":57, "b1":58, "c1":59, "d1":60, "e1":61, "f1":62, "g1":63, "h1":64};
 
-const pos_arr = ["a8", "b8", "c8", "d8", "e8", "f8", "g8", "h8",
+const pos_dict = {
+"a8":0, "b8":1, "c8":2, "d8":3, "e8":4, "f8":5, "g8":6, "h8":7,
+"a7":8, "b7":9, "c7":10, "d7":11, "e7":12, "f7":13, "g7":14, "h7":15,
+"a6":16, "b6":17, "c6":18, "d6":19, "e6":20, "f6":21, "g6":22, "h6":23,
+"a5":24, "b5":25, "c5":26, "d5":27, "e5":28, "f5":29, "g5":30, "h5":31,
+"a4":32, "b4":33, "c4":34, "d4":35, "e4":36, "f4":37, "g4":38, "h4":39,
+"a3":40, "b3":41, "c3":42, "d3":43, "e3":44, "f3":45, "g3":46, "h3":47,
+"a2":48, "b2":49, "c2":50, "d2":51, "e2":52, "f2":53, "g2":54, "h2":55,
+"a1":56, "b1":57, "c1":58, "d1":59, "e1":60, "f1":61, "g1":62, "h1":63};
+
+const pos_arr = [
+"a8", "b8", "c8", "d8", "e8", "f8", "g8", "h8",
 "a7", "b7", "c7", "d7", "e7", "f7", "g7", "h7",
 "a6", "b6", "c6", "d6", "e6", "f6", "g6", "h6",
 "a5", "b5", "c5", "d5", "e5", "f5", "g5", "h5",
@@ -146,6 +169,7 @@ function getWhiteBoard() {
         </tr>
     </table>`
     addMovable();
+    documentListener();
     //addDetect();
 }
 
@@ -236,7 +260,29 @@ function getBlackBoard() {
         </tr>
     </table>`
     addMovable();
+    documentListener();
     //addDetect();
+}
+
+function documentListener() {
+    document.addEventListener("mousemove", function() {getElement(event);setLatestPiece()});
+    document.addEventListener("click", function() {
+        console.log("click event");
+        if (clicked && !isSameCell()) {
+            console.log("changed?");
+            console.log(selectedPiece);
+            changeParent();
+        }
+    });
+}
+
+function getRandomBoard() {
+    let randomNumber =  Math.floor(Math.random() * 2);
+    if (randomNumber == 0) {
+        getBlackBoard();
+    } else {
+        getWhiteBoard();
+    }
 }
 
 function addMovable() {
@@ -245,18 +291,72 @@ function addMovable() {
         const id = pos_arr[i];
         const piece = document.querySelector(`#${id} > img`);
         let down = false;
+        let toggleClick = false;
         if (piece != null) {
-            piece.addEventListener("mouseenter", function() {console.log("Enter")});
-            piece.addEventListener("mousedown", function() {down = true;selectedPiece=piece;selectedPiece.style.transform=`translate(${event.clientX-selectedPiece.parentNode.getBoundingClientRect().left-realsize/2}px,${event.clientY-selectedPiece.parentNode.getBoundingClientRect().top-realsize/2}px)`;piece.style.zIndex="2"});
+            piece.addEventListener("mousedown", function() {down = true;selectedPiece=piece;setLatestPiece();moveSelectedToMouse();piece.style.zIndex="2"});
             piece.addEventListener("mousemove", function() {moveWithPointer(down, piece, event)});
-            piece.addEventListener("mouseup", function() {down = false;if(selectedPiece!=null){changeParent()};piece.style.zIndex="1";selectedPiece=null;});
-            piece.addEventListener("mouseleave", function() {piece.style.zIndex="1";down = false;if(selectedPiece!=null){selectedPiece.style.transform=`translate(${selectedPiece.offsetLeft}px,${selectedPiece.offsetTop})`};selectedPiece=null});
-            //piece.addEventListener("mouseleave", function() {piece.style.zIndex="1";console.log("left now");if(selectedPiece!=null){console.log("left");selectedPiece.style.transform=`translate(${event.clientX-selectedPiece.parentNode.getBoundingClientRect().left-realsize/2}px,${event.clientY-selectedPiece.parentNode.getBoundingClientRect().top-realsize/2}px)`;console.log("after")}});
+            piece.addEventListener("mouseup", function() {
+                if (isSameCell()) {
+                    console.log("same cell")
+                    down = false;
+                    if (toggleClick) {
+                        console.log("was clicked");
+                        clicked = false;
+                        toggleClick = false;
+                        moveSelectedToParent();
+                        selectedPiece = null;
+                    } else {
+                        console.log("was not clicked");
+                        clicked = true;
+                        toggleClick = true;
+                        moveSelectedToParent();
+                        selectedPiece = piece;
+                        console.log(selectedPiece);
+                    }
+                } else {
+                    down = false;
+                    if(selectedPiece!=null){
+                        changeParent()
+                    };
+                    piece.style.zIndex="1";
+                    selectedPiece=null;
+                }
+            });
+            // this null check is used in order to not cause error in console due to bug
+            piece.addEventListener("mouseleave", function() {
+                piece.style.zIndex="1";
+                down = false;
+                //if(selectedPiece!=null){moveSelectedToParent()};selectedPiece=null
+            });
 
-            document.addEventListener("mousemove", function() {getElement(event);});
-            
         }
     }
+}
+
+function setLatestPiece() {
+    if (selectedPiece != null) {
+        latestPiece = selectedPiece;
+    } 
+}
+
+function isSameCell() {
+    //console.log(selectedCell);
+    //console.log(latestPiece.parentNode);
+    //console.log("-----")
+    let theBool = selectedCell == latestPiece.parentNode;
+    //console.log(theBool);
+    return theBool;
+    
+}
+
+function moveSelectedToMouse() {
+    let parentX = selectedPiece.parentNode.getBoundingClientRect().left;
+    let parentY = selectedPiece.parentNode.getBoundingClientRect().top;
+    selectedPiece.style.transform=`translate(${event.clientX-parentX-realsize/2}px,${event.clientY-parentY-realsize/2}px)`
+}
+
+function moveSelectedToParent() {
+    selectedPiece.style.transform=`translate(${selectedPiece.offsetLeft}px,${selectedPiece.offsetTop})`
 }
 
 function moveWithPointer(down, piece, event) {
@@ -270,6 +370,7 @@ function moveWithPointer(down, piece, event) {
         let yc = event.clientY - rect.top - realsize/2;
         let xpos = piece.offsetLeft;
         let ypos = piece.offsetTop;
+        console.log(`xpos ${xpos} ypos ${ypos}`)
         xdist = (xc - xpos);
         ydist = (yc - ypos);
         piece.style.transform = `translate(${xdist}px,${ydist}px)`;
@@ -282,20 +383,40 @@ function changeParent() {
     if (selectedCell.innerHTML.trim()!="") {
         console.log("not null");
         if (canCapture) {
-            console.log(selectedCell)
+            //console.log(selectedCell)
             while (selectedCell.firstChild) {
                 selectedCell.removeChild(selectedCell.firstChild);
             }
-            console.log("outer" + selectedCell.outerHTML);
         }
     }
-    console.log(selectedCell);
+    //console.log(selectedCell);
     newParent = selectedCell;
-    console.log(newParent);
-    console.log(selectedPiece);
+    //console.log(newParent);
+    //console.log(selectedPiece);
     newParent.appendChild(selectedPiece);
-    console.log(`Changed: ${newParent}`);
+    console.log(`Changed: ${newParent.innerHTML}`);
     selectedPiece.style.transform = `translate(${selectedPiece.offsetLeft}px,${selectedPiece.offsetTop})`;
+}
+
+function changeLatestPieceParent() {
+    let newParent;
+    if (selectedCell.innerHTML.trim()!="") {
+        // if the cell is not empty and can capture
+        console.log("not null");
+        if (canCapture) {
+            //console.log(selectedCell)
+            while (selectedCell.firstChild) {
+                selectedCell.removeChild(selectedCell.firstChild);
+            }
+        }
+    }
+    //console.log(selectedCell);
+    newParent = selectedCell;
+    //console.log(newParent);
+    //console.log(selectedPiece);
+    newParent.appendChild(latestPiece);
+    console.log(`Changed: ${newParent.innerHTML}`);
+    latestPiece.style.transform = `translate(${latestPiece.offsetLeft}px,${latestPiece.offsetTop})`;
 }
 
 function getElement(event) {
@@ -309,32 +430,5 @@ function getElement(event) {
     }
 }
 
-/*
-function addDetect() {
-    for (let i=0; i<64; i++) {
-        const id = pos_arr[i];
-        const box = document.getElementById(id);
-        let on = false;
-        box.addEventListener("mouseover", function() {on = true;console.log(`enter ${id} ` + on)});
-        box.addEventListener("mouseleave", function() {on = false;console.log(`leave ${id} ` + on)});
-        box.addEventListener("mouseup", function() {console.log(`up ${id}`);detectRelease(on, id)});
-    }
-}
-
-function detectRelease(on, id) {
-    if (on) {
-        console.log("box " + id)
-        console.log(selectedPiece);
-        const box = document.getElementById(id);
-        console.log(box);
-        box.appendChild(selectedPiece);
-        let rect = box.getBoundingClientRect();
-        let xdist = selectedPiece.offsetLeft;
-        let ydist = selectedPiece.offsetTop;
-        selectedPiece.style.transform = `translate(${xdist}px,${ydist}px)`;
-        selectedPiece = null;
-    }
-    
-}*/
 
 //match (:hover?)
